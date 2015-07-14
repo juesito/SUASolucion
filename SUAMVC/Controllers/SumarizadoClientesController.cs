ca2002cf5de3bc276160e46879c85a09de474c8f
Lea14 <leona_queen@hotmail.com>
2015-07-14 10:35:21 -0500

    leo
    leo comments

---
+++ SUAMVC/Controllers/SumarizadoClientesController.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SUADATOS;
+using System.Data.SqlClient;
+using System.Web.Helpers;
+
+
+namespace SUAMVC.Controllers
+{
+    public class SumarizadoClientesController : Controller
+    {
+        private suaEntities db = new suaEntities();
+
+        // GET: SumarizadoClientes
+        public ActionResult Index(String plazasId, String patronesId, String periodoId,
+            String ejercicioId, String clientesId, String usuarioId)
+        {
+            if (!String.IsNullOrEmpty(clientesId))
+            {
+                ViewBag.filtered = true;
+                int clienteId = int.Parse(clientesId.Trim());
+                int userId = int.Parse(usuarioId.Trim());
+
+                int result = db.Database.ExecuteSqlCommand("sp_SumarizadoClientes @usuarioId, @clienteId", new SqlParameter("@usuarioId", userId), new SqlParameter("@clienteId", clienteId));
+                var sumarizadoClientes = db.SumarizadoClientes.Include(s => s.Cliente).Include(s => s.Patrone).Include(s => s.Usuario);
+                if (!String.IsNullOrEmpty(plazasId))
+                {
+                    int plazaTempId = int.Parse(plazasId.Trim());
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.Patrone.Plaza_id.Equals(plazaTempId));
+                }
+                if (!String.IsNullOrEmpty(patronesId))
+                {
+                    int patronesTempId = int.Parse(patronesId);
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.Patrone.Id.Equals(patronesTempId));
+                }
+                if (!String.IsNullOrEmpty(periodoId))
+                {
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.mes.Trim().Equals(periodoId.Trim()));
+                }
+                if (!String.IsNullOrEmpty(ejercicioId))
+                {
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.anno.Trim().Equals(ejercicioId));
+                }
+
+                sumarizadoClientes = sumarizadoClientes.OrderBy(p => p.Patrone.registro);
+                return View(sumarizadoClientes.ToList());
+            }
+            return View();
+        }
+
+        // GET: SumarizadoClientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SumarizadoCliente sumarizadoCliente = db.SumarizadoClientes.Find(id);
+            if (sumarizadoCliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sumarizadoCliente);
+        }
+
+        // GET: SumarizadoClientes/Create
+        public ActionResult Create()
+        {
+            ViewBag.clienteId = new SelectList(db.Clientes, "Id", "claveCliente");
+            ViewBag.patronId = new SelectList(db.Patrones, "Id", "registro");
+            ViewBag.usuarioId = new SelectList(db.Usuarios, "Id", "nombreUsuario");
+            return View();
+        }
+
+        // POST: SumarizadoClientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,patronId,anno,mes,imss,rcv,infonavit,total,nt,usuarioId,fechaCreacion,clienteId")] SumarizadoCliente sumarizadoCliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SumarizadoClientes.Add(sumarizadoCliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.clienteId = new SelectList(db.Clientes, "Id", "claveCliente", sumarizadoCliente.clienteId);
+            ViewBag.patronId = new SelectList(db.Patrones, "Id", "registro", sumarizadoCliente.patronId);
+            ViewBag.usuarioId = new SelectList(db.Usuarios, "Id", "nombreUsuario", sumarizadoCliente.usuarioId);
+            return View(sumarizadoCliente);
+        }
+
+        // GET: SumarizadoClientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SumarizadoCliente sumarizadoCliente = db.SumarizadoClientes.Find(id);
+            if (sumarizadoCliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.clienteId = new SelectList(db.Clientes, "Id", "claveCliente", sumarizadoCliente.clienteId);
+            ViewBag.patronId = new SelectList(db.Patrones, "Id", "registro", sumarizadoCliente.patronId);
+            ViewBag.usuarioId = new SelectList(db.Usuarios, "Id", "nombreUsuario", sumarizadoCliente.usuarioId);
+            return View(sumarizadoCliente);
+        }
+
+        // POST: SumarizadoClientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,patronId,anno,mes,imss,rcv,infonavit,total,nt,usuarioId,fechaCreacion,clienteId")] SumarizadoCliente sumarizadoCliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sumarizadoCliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.clienteId = new SelectList(db.Clientes, "Id", "claveCliente", sumarizadoCliente.clienteId);
+            ViewBag.patronId = new SelectList(db.Patrones, "Id", "registro", sumarizadoCliente.patronId);
+            ViewBag.usuarioId = new SelectList(db.Usuarios, "Id", "nombreUsuario", sumarizadoCliente.usuarioId);
+            return View(sumarizadoCliente);
+        }
+
+        // GET: SumarizadoClientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SumarizadoCliente sumarizadoCliente = db.SumarizadoClientes.Find(id);
+            if (sumarizadoCliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sumarizadoCliente);
+        }
+
+        // POST: SumarizadoClientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SumarizadoCliente sumarizadoCliente = db.SumarizadoClientes.Find(id);
+            db.SumarizadoClientes.Remove(sumarizadoCliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public void GetExcel(String plazasId, String patronesId, String periodoId,
+            String ejercicioId, String clientesId, String usuarioId)
+        {
+            var sumarizadoClientes = db.SumarizadoClientes.Include(s => s.Cliente).Include(s => s.Patrone).Include(s => s.Usuario);
+            if (!String.IsNullOrEmpty(clientesId))
+            {
+                ViewBag.filtered = true;
+                int clienteId = int.Parse(clientesId.Trim());
+                int userId = int.Parse(usuarioId.Trim());
+
+                int result = db.Database.ExecuteSqlCommand("sp_SumarizadoClientes @usuarioId, @clienteId", new SqlParameter("@usuarioId", userId), new SqlParameter("@clienteId", clienteId));
+                sumarizadoClientes = db.SumarizadoClientes.Include(s => s.Cliente).Include(s => s.Patrone).Include(s => s.Usuario);
+                if (!String.IsNullOrEmpty(plazasId))
+                {
+                    int plazaTempId = int.Parse(plazasId.Trim());
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.Patrone.Plaza_id.Equals(plazaTempId));
+                }
+                if (!String.IsNullOrEmpty(patronesId))
+                {
+                    int patronesTempId = int.Parse(patronesId);
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.Patrone.Id.Equals(patronesTempId));
+                }
+                if (!String.IsNullOrEmpty(periodoId))
+                {
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.mes.Trim().Equals(periodoId.Trim()));
+                }
+                if (!String.IsNullOrEmpty(ejercicioId))
+                {
+                    sumarizadoClientes = sumarizadoClientes.Where(s => s.anno.Trim().Equals(ejercicioId));
+                }
+            }
+
+            sumarizadoClientes = sumarizadoClientes.OrderBy(p => p.Patrone.registro);
+            List<SumarizadoCliente> allCust = new List<SumarizadoCliente>();
+
+            allCust = sumarizadoClientes.ToList();
+
+            WebGrid grid = new WebGrid(source: allCust, canPage: false, canSort: false);
+
+            List<WebGridColumn> gridColumns = new List<WebGridColumn>();
+
+            gridColumns.Add(grid.Column("Patrone.registro", "Reg. Patronal", null, null, true));
+            gridColumns.Add(grid.Column("anno", "Año", null, null, true));
+            gridColumns.Add(grid.Column("mes", "Mes", null, null, true));
+            gridColumns.Add(grid.Column("Cliente.claveCliente", "ID. Cliente", null, null, true));
+            gridColumns.Add(grid.Column("Cliente.descripcion", "Nombre", null, null, true));
+            gridColumns.Add(grid.Column("Cliente.Grupos.claveGrupo", "ID. Grupo", null, null, true));
+            gridColumns.Add(grid.Column("imss", "IMSS", null, null, true));
+            gridColumns.Add(grid.Column("rcv", "RCV", null, null, true));
+            gridColumns.Add(grid.Column("infonavit", "Infonavit", null, null, true));
+            gridColumns.Add(grid.Column("total", "Total", null, null, true));
+            gridColumns.Add(grid.Column("nt", "NT", null, null, true));
+            gridColumns.Add(grid.Column("Patrone.Plaza.cveCorta", "Plaza", null, null, true));
+
+            string gridData = grid.GetHtml(
+                columns: grid.Columns(gridColumns.ToArray())
+                    ).ToString();
+
+            Response.ClearContent();
+            DateTime date = DateTime.Now;
+            String fileName = "ClientesPagos-" + date.ToString("ddMMyyyyHHmm") + ".xls";
+            Response.AddHeader("content-disposition", "attachment; filename=" + fileName);
+            Response.ContentType = "application/excel";
+            Response.Write(gridData);
+            Response.End();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SUAMVC/Helpers/LinqToExcelProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace SUAMVC.Helpers
+{
+    public class LinqToExcelProvider
+    {
+        /// <summary>
+        /// Obtenemos el nombre de archivo excel
+        /// </summary>
+        private string FileName { get; set; }
+
+        /// <summary>
+        /// Template connectionstring para Excel connections
+        /// esta cadena es para archivos excel 2007 y 2010
+        /// </summary>
+        private const string ConnectionStringTemplateOld = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=Excel 8.0;HDR=YES;IMEX=1;";
+        //
+        private const string ConnectionStringTemplate = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 12.0 Xml;";
+        //para archivos de 97-2003 usar la siguiente cadena
+        //private string ConnectionStringTemplateVeryOld = "provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=Excel 8.0;";
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="fileName">The Excel file to process</param>
+        public LinqToExcelProvider(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        /// <summary>
+        /// Returns a worksheet as a linq-queryable enumeration
+        /// </summary>
+        /// <param name="sheetName">The name of the worksheet</param>
+        /// <returns>An enumerable collection of the worksheet</returns>
+        public EnumerableRowCollection<DataRow> GetWorkSheet(string sheetName)
+        {
+            // Build the connectionstring
+            string connectionString = string.Format(ConnectionStringTemplate, FileName);
+
+            // Query the specified worksheet
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(string.Format("SELECT * FROM [{0}$]", sheetName), connectionString);
+
+            // Fill the dataset from the data adapter
+            DataSet myDataSet = new DataSet();
+            dataAdapter.Fill(myDataSet, "ExcelInfo");
+            //dataAdapter.Fill(myDataSet);
+
+            // Initialize a data table which we can use to enumerate the contents based on the dataset
+            DataTable dataTable = myDataSet.Tables["ExcelInfo"];
+
+            // Return the data table contents as a queryable enumeration
+            return dataTable.AsEnumerable();
+        }
+    }
+}
