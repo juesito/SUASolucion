4e420d636b08b31e9b675492a19bd40934168618
Lea14 <leona_queen@hotmail.com>
2015-07-14 10:48:13 -0500

    prueba

--- SUAMVC/Controllers/PagosController.cs
+++ SUAMVC/Controllers/PagosController.cs
@@ -170,7 +170,7 @@ namespace SUAMVC.Controllers
 
                                 Asegurado asegurado = db.Asegurados.Where(a => a.numeroAfiliacion.Equals(nss.Trim())).FirstOrDefault();
 
-                                detallePago = db.DetallePagoes.Where(dp => dp.pagoId.Equals(pago.id) && dp.aseguradoId.Equals(asegurado.id)).FirstOrDefault();
+                                detallePago = db.DetallePagos.Where(dp => dp.pagoId.Equals(pago.id) && dp.aseguradoId.Equals(asegurado.id)).FirstOrDefault();
 
                                 if (String.IsNullOrEmpty(detallePago.Asegurado.nombre))
                                 {
@@ -392,7 +392,7 @@ namespace SUAMVC.Controllers
                                         db.Entry(detallePago).State = EntityState.Modified;
                                     }
                                     else {
-                                        db.DetallePagoes.Add(detallePago);
+                                        db.DetallePagos.Add(detallePago);
                                     }
                                     
                                     db.SaveChanges();
@@ -466,7 +466,7 @@ namespace SUAMVC.Controllers
 
             Pago pago = db.Pagos.Where(p => p.id.Equals(id)).FirstOrDefault();
 
-            List<DetallePago> detallePago = db.DetallePagoes.Where(r => r.pagoId.Equals(id)).ToList();
+            List<DetallePago> detallePago = db.DetallePagos.Where(r => r.pagoId.Equals(id)).ToList();
 
             resumenPagoModel.pago = pago;
             resumenPagoModel.detalle = detallePago;
